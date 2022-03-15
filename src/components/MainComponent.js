355ecd8452f0595aee9ab37032a2d5721aa1a53c
Alexandro <Alvasoul92@yahoo.com>
2022-03-15 00:49:11 -0700

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./DirectoryComponent";
-import CampsiteInfo from './CampsiteInfoComponent'
+import CampsiteInfo from "./CampsiteInfoComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
@@ -9,25 +10,30 @@ class Main extends Component {
     super(props);
     this.state = {
       campsites: CAMPSITES,
-      selectedCampsite: null
+      selectedCampsite: null,
     };
   }
 
   onCampsiteSelect(campsiteId) {
-    this.setState({selectedCampsite: campsiteId});
-}
-
+    this.setState({ selectedCampsite: campsiteId });
+  }
 
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
-        <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+        <Header />
+        <Directory
+          campsites={this.state.campsites}
+          onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+        <Footer />
       </div>
     );
   }
