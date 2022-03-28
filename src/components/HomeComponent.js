72567d5ee9088148ab36fb7456526a51e2285a5f
Alexandro <Alvasoul92@yahoo.com>
2022-03-27 17:56:01 -0700

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -33,7 +33,9 @@ function Home(props) {
                         errMess={props.campsitesErrMess} />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion}
+                    isLoading={props.promotionLoading}
+                    errMess={props.promotionErrMess} />
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.partner} />
